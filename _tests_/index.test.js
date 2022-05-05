9ebd3c6d8dd8656fbd530ebec1818424b020dda8
alexeilozhkin <alexeilozhkin983@gmail.com>
2022-05-06 00:59:09 +0400

    step 4

--- _tests_/index.test.js
+++ _tests_/index.test.js
@@ -1,11 +1,16 @@
-import fs from 'fs';
-import path, { dirname } from 'path';
 import genDiff from '../src/index.js';
 
-const file1path = './_fixtures_/file1.json';
-const file2path = './_fixtures_/file2.json';
-const expectedString = '{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+const file1Path = './_fixtures_/file1.json';
+const file2Path = './_fixtures_/file2.json';
+const expectedString = `{
+  - follow: false
+    host: hexlet.io
+  - proxy: 123.234.53.22
+  - timeout: 50
+  + timeout: 20
+  + verbose: true
+}`;
 
 test('gendiff', () => {
-  expect(genDiff(file1path, file2path)).toEqual(expectedString);
+  expect(genDiff(file1Path, file2Path)).toBe(expectedString);
 });
